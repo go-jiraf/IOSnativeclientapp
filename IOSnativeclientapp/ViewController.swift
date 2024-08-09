02f6ef37098c3d2b28e0ef99eb370db322dd7b02
mariano.a.dim <mariano.a.dim@gmail.com>
2024-08-09 14:14:18 -0300

    Initial Commit

---
+++ IOSnativeclientapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSnativeclientapp
+//
+//  Created by Mariano Andres Di Maggio on 09/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
