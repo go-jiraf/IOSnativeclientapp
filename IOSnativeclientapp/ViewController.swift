4a9a7f882031161639069115efcea5aeddb9fd7d
mariano.a.dim <mariano.a.dim@gmail.com>
2024-08-09 15:00:36 -0300

    first commit

--- IOSnativeclientapp/ViewController.swift
+++ IOSnativeclientapp/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func openWebPage(_ sender: UIButton) {
+        // The URL you want to open
+        if let url = URL(string: "https://video-commerce.vercel.app/company/just?category=belleza") {
+            // Create an instance of SFSafariViewController
+            let safariViewController = SFSafariViewController(url: url)
+            
+            // Optional: Customize the appearance
+            safariViewController.preferredBarTintColor = UIColor.black
+            safariViewController.preferredControlTintColor = UIColor.white
+            
+            // Set the delegate
+            safariViewController.delegate = self
+            
+            // Present the Safari view controller
+            present(safariViewController, animated: true, completion: nil)
+        }
+    }
 }
 
+// Extend the ViewController to conform to SFSafariViewControllerDelegate
+extension ViewController: SFSafariViewControllerDelegate {
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        // Handle the event when the SafariViewController is dismissed
+        dismiss(animated: true, completion: nil)
+    }
+}
